de875b185ddb5a5ac25a5ffb894f6732dbc946ce
Emelie Sannerfelt <emelie.sannerfelt@hotmail.com>
2021-02-17 11:57:20 +0100

    Exempel 8 övning

---
+++ example8.java
@@ -0,0 +1,18 @@
+public class example8{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        System.out.println(b);
+        if((a==10) && (b==20)){
+
+            a= 20;
+            b= 10;
+            System.out.println(a);
+            System.out.println(b);
+        }
+        else{
+            System.out.println("Fel");
+            }
+    }
+}
