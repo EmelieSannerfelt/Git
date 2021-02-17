f83593822e1c7a175321cb74b9f80a96c169ca70
Emelie Sannerfelt <emelie.sannerfelt@hotmail.com>
2021-02-17 11:28:38 +0100

    Skapade symbol

--- example6.java
+++ example6.java
@@ -1,5 +1,9 @@
 public class example6{
     public static void main(String[] args){
+        System.out.println("xxxxx");
+        System.out.println("x   x");
+        System.out.println("x   x");
+        System.out.println("xxxxx");
 
     }
 }
